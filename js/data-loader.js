47ca7083627a207d9ea2bc14bcc050862abe1928
uruja <uruja@student.kit.edu>
2026-05-11 19:54:02 +0200

    Update data-loader.js

--- js/data-loader.js
+++ js/data-loader.js
@@ -7,7 +7,7 @@ class DataLoader {
     async loadCatalog() {
         try {
             const response = await fetch(`${CONFIG.dataBaseUrl}/catalog.json`);
-            if (!response.ok) throw new Error(`Failed to load ${CONFIG.dataBaseUrl}/catalog.json`);
+            if (!response.ok) throw new Error(`Failed to load burdiiiiiiiiiiiiiiii ${CONFIG.dataBaseUrl}/catalog.json`);
             this.catalog = await response.json();
             return this.catalog;
         } catch (error) {
